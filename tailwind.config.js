7c37ca4d39c97acfa2a8ae63e4aad90668ba3063
iammarkps <markkung08@gmail.com>
2020-09-05 23:46:41 +0700

    :tada: Migrate blog

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,4 +30,49 @@ module.exports = {
       },
     },
   },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/typography'),
+    function ({ addBase, addComponents, theme }) {
+      addBase([
+        {
+          '@font-face': {
+            fontFamily: 'Inter var',
+            fontWeight: '100 900',
+            fontStyle: 'normal',
+            fontNamedInstance: 'Regular',
+            fontDisplay: 'swap',
+            src: 'url("/fonts/Inter-roman.var.woff2?3.13") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'Inter var',
+            fontWeight: '100 900',
+            fontStyle: 'italic',
+            fontNamedInstance: 'Italic',
+            fontDisplay: 'swap',
+            src: 'url("/fonts/Inter-italic.var.woff2?3.13") format("woff2")',
+          },
+        },
+        {
+          '@font-face': {
+            fontFamily: 'Kanit',
+            fontStyle: 'normal',
+            fontWeight: '400',
+            fontDisplay: 'swap',
+            src:
+              "local('Kanit Regular'), local('Kanit-Regular'), url('https://fonts.gstatic.com/s/kanit/v5/nKKZ-Go6G5tXcraBGwCKd6xBDFs.woff2') format('woff2')",
+            unicodeRange: 'U+0E01-0E5B, U+200C-200D, U+25CC',
+          },
+          '@font-face': {
+            fontFamily: 'Sukhumvit Set',
+            fontDisplay: 'swap',
+            src: "local('Sukhumvit Set')",
+            unicodeRange: 'U+0E01-0E5B, U+200C-200D, U+25CC',
+          },
+        },
+      ])
+    },
+  ],
 }
